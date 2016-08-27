80d38d43b66d0940a25e5fc8a549366205ae8ac6
Annika Klopfer <annika.klopfer@gmail.com>
2016-08-27 15:10:28 -0700

    updating users table
    adding first name, last name, email, and password digest to users.
    Password digest is storing the hashed version of the password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624061203) do
+ActiveRecord::Schema.define(version: 20160827220535) do
 
   create_table "comments", force: true do |t|
     t.string   "owner"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20160624061203) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
