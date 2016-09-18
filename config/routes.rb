afa9f1f62ca1487f1312e2c82c8e8f9aede5f3a1
Annika Klopfer <annika.klopfer@gmail.com>
2016-09-17 21:49:28 -0700

    Signup ability
    Added users and the ability to sign up for an account

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Blog1::Application.routes.draw do
     resources :comments
   end
 
+  get 'signup'  => 'users#new' 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827220535) do
+ActiveRecord::Schema.define(version: 20160914043045) do
 
   create_table "comments", force: true do |t|
     t.string   "owner"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160827220535) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "password_confirmation"
   end
 
 end
